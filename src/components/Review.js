5bfaace49ab9a6418471aa4b8ae1e2687a27a3a5
Shaban Irshad <shabanirshad408@gmail.com>
2026-01-19 14:04:44 +0530

    try

---
+++ src/components/Review.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Review=({review})=>{
+    // console.log(review);
+    return(
+        <>
+            <div className="review">
+                <h2 className='author'>{review.name}</h2>
+                <p className='job'>{review.job}</p>
+                <img  className='person-img' src={review.image} alt='picture'/>
+                <p className='info'>{review.text}</p>
+                <button className='prev-btn'>Previous</button>
+                <button className='next-btn'> Next</button>
+                <button className='random-btn'>surprise me</button>
+            </div>
+        </>
+    );
+}
+export default Review;
